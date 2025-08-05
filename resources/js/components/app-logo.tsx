019fb4158159daae629e074577eb91bcd8e2c1e6
kynth65 <kynth65@gmail.com>
2025-08-05 15:23:22 +0800

    Add icon logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,10 @@
-import AppLogoIcon from './app-logo-icon';
+import AppLogoIcon from '../../images/logo.png';
 
 export default function AppLogo() {
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <img src={AppLogoIcon} alt="" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">MONOMEMO</span>
